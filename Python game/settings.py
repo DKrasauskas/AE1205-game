097276515ff319e4508edd96e7534481e2cdb178
Dominykas <145802287+DKrasauskas@users.noreply.github.com>
2024-06-07 20:34:58 +0300

    Add files via upload

---
+++ Python game/settings.py
@@ -0,0 +1,19 @@
+import pygame as py
+
+SCR_WIDTH = 800
+SCR_HEIGHT = 1200
+FLOOR_HEIGHT = 600
+
+player1 = (
+py.K_LEFT, 
+py.K_UP, 
+py.K_RIGHT, 
+py.K_DOWN
+)
+
+player2 = (
+py.K_a, 
+py.K_w, 
+py.K_d, 
+py.K_s
+)
